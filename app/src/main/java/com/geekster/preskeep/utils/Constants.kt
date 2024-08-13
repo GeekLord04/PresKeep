c3e1dba36fd47ebcafd4285598fa0652b88f7c4e
Chirag Chakraborty <chiragofficial04@gmail.com>
2024-08-14 02:46:21 +0530

    BuildConfig fields added

--- app/src/main/java/com/geekster/preskeep/utils/Constants.kt
+++ app/src/main/java/com/geekster/preskeep/utils/Constants.kt
@@ -8,11 +8,6 @@ object Constants {
     const val TAG = "PRESKEEP_TAG"
     const val BASE_URL = "https://cloud.appwrite.io/v1"
 
-//    const val PROJECT_ID = "65a2cbc06a988efed38b"
-//    const val PREFS_TOKEN_FILE = "PREFS_TOKEN_FILE"
-//    const val DATABASE_ID = "65f1efc89d9549a17e06"
-//    const val COLLECTION_ID = "65f1efd29ed9d3f748ac"
-
     const val PROJECT_ID = BuildConfig.PROJECT_ID
     const val PREFS_TOKEN_FILE = BuildConfig.PREFS_TOKEN_FILE
     const val DATABASE_ID = BuildConfig.DATABASE_ID
